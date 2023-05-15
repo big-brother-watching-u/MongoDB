3d32cc752ac5506223f720a06a7101450773d61e
Ilya Bubekov <bubiekov@gmail.com>
2023-05-15 11:48:56 +0300

    Create DB and collections

--- MongoDB/Program.cs
+++ MongoDB/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Driver;
 
 namespace MongoDB
 {
@@ -10,6 +11,23 @@ namespace MongoDB
     {
         static void Main(string[] args)
         {
+            var program = new Program();
+            program.DB();
+            Console.ReadKey();
+        }
+        async void DB()
+        {
+
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("test");
+            //await db.CreateCollectionAsync("people"); //создание коллекции people
+            //await db.RenameCollectionAsync("people", "users"); //переименовать из people в users
+            var collections = await db.ListCollectionsAsync();
+            foreach (var collection in collections.ToList())
+            {
+                Console.WriteLine(collection);
+            }
+
         }
     }
 }
