c3840f814a91e175c834561e231ca7028d4df343
Ilya Bubekov <bubiekov@gmail.com>
2023-05-15 16:57:52 +0300

    Add Object

--- MongoDB/Program.cs
+++ MongoDB/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace MongoDB
@@ -20,14 +21,55 @@ namespace MongoDB
 
             MongoClient client = new MongoClient("mongodb://localhost:27017");
             var db = client.GetDatabase("test");
-            //await db.CreateCollectionAsync("people"); //создание коллекции people
-            //await db.RenameCollectionAsync("people", "users"); //переименовать из people в users
+            //await db.CreateCollectionAsync("people"); //создать коллекции people
+            //----------------------------------------------------------------------------------------------------------------------------------------
+
+            //await db.RenameCollectionAsync("people", "users"); //переименовать коллекцию из people в users
+            //----------------------------------------------------------------------------------------------------------------------------------------
+
+            //await db.DropCollectionAsync("users");  //удалить коллекцию users
+            //----------------------------------------------------------------------------------------------------------------------------------------
+
+            /*
             var collections = await db.ListCollectionsAsync();
             foreach (var collection in collections.ToList())
             {
-                Console.WriteLine(collection);
+                Console.WriteLine(collection); //Получение и вывод в консоль списка коллекций 
             }
+            */
+            //----------------------------------------------------------------------------------------------------------------------------------------
+
+            /*
+            BsonDocument doc = new BsonDocument
+            {
+                {"name","Tom"},
+
+                {"age", 38},
+
+                {"company", new BsonDocument{
+                    {"initial", new BsonDocument{ //Создание документа  и его последующий вывод
+                        {"name", "Microsoft"}
+                    }}
+                }},
+                {"languages", new BsonArray{"english", "german", "spanish" } }
+            };
+            Console.WriteLine(doc["company"]["initial"]["name"]);
+            */
+            //----------------------------------------------------------------------------------------------------------------------------------------
+
 
         }
+    }
+    class Person
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Company Company { get; set; }
+        public List<string> Languages { get; set; } = new List<string>();
+    }
+    class Company
+    {
+        public string Name { get; set; }
     }
 }
